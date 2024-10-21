a014ed86365c14ea0adef6254f352cace3479f72
SHIVANSHGARG07 <122355583+SHIVANSHGARG07@users.noreply.github.com>
2024-10-22 00:55:26 +0530

    Update Contact.jsx

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://portfolio-backend-nu-ashy.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
